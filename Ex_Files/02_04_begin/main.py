0d411472ec301782c8d4cef23fd105accca120de
Joshua Uzell <80922592+JoshuaUzell@users.noreply.github.com>
2024-09-13 13:23:01 +0000

    For 02_04_begin, added a while loop that iterates through ages, a for in loop for printing out names and ages together in reversed order and a enumerate example

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -6,16 +6,28 @@ while i < len(NAMES):
     print(NAMES[i], AGES[i])
     i += 1
 
+age_count = 0
+while age_count < len(AGES):
+    print(AGES[age_count])
+    age_count += 1
+
 for name in NAMES:
     print(name)
 
+#This is how you go through 2 lists at once
 for name, age in zip(NAMES, AGES):
     print(f"{name} {age}")
 
+#Goes through a reversed collection
 for name in reversed(NAMES):
     print(name)
 
 for i in range(5):
     print(i)
 
-# enumerate
+for name, age in zip(reversed(NAMES), reversed(AGES)):
+    print(f"{name} {age}")
+
+# enumerate - lets you see where exactly you are in terms of the iteration
+for count, name in enumerate(NAMES):
+    print(f"{count} {name}")
